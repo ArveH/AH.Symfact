1c20c7f3316c612aea98d26db79f022bf8427055
Arve Hansen <arve.hansen@unit4.com>
2022-11-26 19:55:10 +0100

    Write to Messages Make it scrollable

--- AH.Symfact.UI/ViewModels/TestingViewModel.cs
+++ AH.Symfact.UI/ViewModels/TestingViewModel.cs
@@ -5,8 +5,12 @@ using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Messaging;
 using Serilog;
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.Threading.Tasks;
+using AH.Symfact.UI.Extensions;
+using CommunityToolkit.Mvvm.Input;
 
 namespace AH.Symfact.UI.ViewModels;
 
@@ -28,19 +32,86 @@ public partial class TestingViewModel : ObservableRecipient
         _tableService = tableService;
         _dbCommands = dbCommands;
         _logger = logger.ForContext<TestingViewModel>();
+        ExecuteSequentialCommand = new AsyncRelayCommand(ExecuteSequentialAsync);
+        ExecuteParallelCommand = new AsyncRelayCommand(ExecuteParallelAsync);
+        ClearMessagesCommand = new RelayCommand(ClearMessages);
     }
 
     [ObservableProperty]
     private string _selectedFile = string.Empty;
     [ObservableProperty]
     private ObservableCollection<string> _queryFiles = new();
+    [ObservableProperty]
+    private ObservableCollection<string> _messages = new();
+    [ObservableProperty]
+    private int _sequentialCount = 10;
+    partial void OnSequentialCountChanging(int value)
+    {
+        Messages.Add($"SequentialCount changed to {value}");
+    }
+
+    [ObservableProperty]
+    private int _parallelCount = 10;
+    partial void OnParallelCountChanging(int value)
+    {
+        Messages.Add($"ParallelCount changed to {value}");
+    }
 
     public void QueryFileChanged(string? queryFile)
     {
         if (!string.IsNullOrWhiteSpace(queryFile))
         {
             SelectedFile = queryFile;
+            Messages.Add($"Script file = '{SelectedFile}'");
         }
+    }
+
+    public IAsyncRelayCommand ExecuteSequentialCommand { get; }
+    public IAsyncRelayCommand ExecuteParallelCommand { get; }
+    public IRelayCommand ClearMessagesCommand { get; }
+
+    private void ClearMessages()
+    {
+        Messages.Clear();
+    }
+
+    private async Task<bool> ExecuteScriptAsync(int index, int total)
+    {
+        try
+        {
+            var script = await File.ReadAllTextAsync(SelectedFile);
+            await _dbCommands.ExecuteScriptAsync(script);
+            _logger.Information("({Index} of {Total}) Script '{FileName}' executed", 
+                index, total, SelectedFile);
+            Messages.Add($"({index} of {total}) Script '{SelectedFile}' executed");
+            return true;
+        }
+        catch (Exception ex)
+        {
+            _logger.Error(ex, "Executing script '{FileName}' ({Index} of {Total}) failed", 
+                SelectedFile, index, total);
+            Messages.Add($"Executing script Script '{SelectedFile}' ({index} of {total}) failed. " + ex.FlattenMessages());
+            return false;
+        }
+    }
+
+    private async Task ExecuteSequentialAsync()
+    {
+        for (var i = 0; i < SequentialCount; i++)
+        {
+            await ExecuteScriptAsync(i+1, SequentialCount);
+        }
+    }
+
+    private async Task ExecuteParallelAsync()
+    {
+        var tasks = new List<Task>();
+        for (var i = 0; i < ParallelCount; i++)
+        {
+            tasks.Add(ExecuteScriptAsync(i+1, ParallelCount));
+        }
+
+        await Task.WhenAll(tasks);
     }
 
     public void SelectQueryFile()
