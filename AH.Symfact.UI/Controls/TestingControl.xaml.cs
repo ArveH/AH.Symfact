14ffe7df3665c125a8fef7d3f95ff1b621acd1e2
Arve Hansen <arve.hansen@unit4.com>
2022-11-26 15:18:46 +0100

    Choose file Combobox

--- AH.Symfact.UI/Controls/TestingControl.xaml.cs
+++ AH.Symfact.UI/Controls/TestingControl.xaml.cs
@@ -1,31 +1,32 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See LICENSE in the project root for license information.
-
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
+using AH.Symfact.UI.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Microsoft.UI.Xaml.Controls;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+namespace AH.Symfact.UI.Controls;
 
-namespace AH.Symfact.UI.Controls
+public sealed partial class TestingControl
 {
-    public sealed partial class TestingControl : UserControl
+    public TestingViewModel ViewModel { get; }
+
+    public TestingControl()
     {
-        public TestingControl()
+        ViewModel = App.Current.Services.GetService<TestingViewModel>()
+                    ?? throw new ApplicationException(
+                        "Can't get ViewModel for TestingControl");
+        this.InitializeComponent();
+    }
+
+    private void OnDropDownOpened(object? sender, object e)
+    {
+        ViewModel.SelectQueryFile();
+    }
+
+    private void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (e.AddedItems.Count > 0)
         {
-            this.InitializeComponent();
+            ViewModel.QueryFileChanged(e.AddedItems[0].ToString());
         }
     }
 }
--- AH.Symfact.UI/ViewModels/TestingViewModel.cs
+++ AH.Symfact.UI/ViewModels/TestingViewModel.cs
@@ -1,7 +1,12 @@
 ﻿using AH.Symfact.UI.Database;
 using AH.Symfact.UI.Services;
+using AH.Symfact.UI.ViewModels.Messages;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Messaging;
 using Serilog;
+using System;
+using System.Collections.ObjectModel;
+using System.IO;
 
 namespace AH.Symfact.UI.ViewModels;
 
@@ -23,5 +28,46 @@ public partial class TestingViewModel : ObservableRecipient
         _tableService = tableService;
         _dbCommands = dbCommands;
         _logger = logger.ForContext<TestingViewModel>();
+    }
+
+    [ObservableProperty]
+    private string _selectedFile = string.Empty;
+    [ObservableProperty]
+    private ObservableCollection<string> _queryFiles = new();
+
+    public void QueryFileChanged(string? queryFile)
+    {
+        if (!string.IsNullOrWhiteSpace(queryFile))
+        {
+            SelectedFile = queryFile;
+        }
+    }
+
+    public void SelectQueryFile()
+    {
+        try
+        {
+            var queryFolder = WeakReferenceMessenger.Default.Send<DataFolderChangedMessage>() + @"\Queries";
+            _logger.Debug("Getting files from {QueryFolder}...", queryFolder);
+            var files = Directory.GetFiles(queryFolder, "*.sql");
+            if (files.Length < 1)
+            {
+                _logger.Information("No files found in folder: '{QueryFolder}'", queryFolder);
+                return;
+            }
+
+            SelectedFile = string.Empty;
+            QueryFiles.Clear();
+            foreach (var file in files)
+            {
+                QueryFiles.Add(file);
+            }
+            _logger.Debug("{FileCount} files found in folder '{QueryFolder}'",
+                files.Length, queryFolder);
+        }
+        catch (Exception ex)
+        {
+            _logger.Error(ex, "Failed to open file picker for XSD files");
+        }
     }
 }
